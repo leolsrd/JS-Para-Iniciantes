d7ea04469259adb38e0b659e4fdad346bf391dcd
Léo Lourenço <leo.lsrd@gmail.com>
2024-06-18 23:04:56 -0300

    feat: Finalizado o exercício 1 do capítulo 9

--- Capitulo-9/script-exercicio-1.js
+++ Capitulo-9/script-exercicio-1.js
@@ -1,3 +1,19 @@
 // Crie uma função que retorne uma Promise que se resolve após 2 segundos
 // com a string "Tempo esgotado!".Use o método setTimeout para simular o
 // atraso.
+
+let myPromise = (time) => {
+  return new Promise((resolve, reject) => {
+    if (time) {
+      setTimeout(resolve, time);
+    } else {
+      reject("Erro");
+    }
+  });
+};
+
+myPromise(2000)
+  .then(function () {
+    console.log("Tempo esgotado!");
+  })
+  .catch((errorMessage) => console.log(errorMessage));
